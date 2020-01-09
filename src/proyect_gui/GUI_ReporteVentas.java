71ea3a2df78a77884e4ee60ca00e3ebdf775372b
Santiago Albuja Boada <jsalbuja1@utpl.edu.ec>
2020-01-09 17:50:44 -0500

    Generación de Reporte de ventas

--- src/proyect_gui/GUI_ReporteVentas.java
+++ src/proyect_gui/GUI_ReporteVentas.java
@@ -5,6 +5,20 @@
  */
 package proyect_gui;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.StringTokenizer;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import proyect_metodos.Entorno;
+
 /**
  *
  * @author HackWhite
@@ -16,6 +30,7 @@ public class GUI_ReporteVentas extends javax.swing.JFrame {
      */
     public GUI_ReporteVentas() {
         initComponents();
+        this.setLocationRelativeTo(null);
     }
 
     /**
@@ -28,8 +43,22 @@ public class GUI_ReporteVentas extends javax.swing.JFrame {
     private void initComponents() {
 
         btn_repVenta_salir = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        fecha_inicio = new javax.swing.JFormattedTextField();
+        jLabel3 = new javax.swing.JLabel();
+        fecha_final = new javax.swing.JFormattedTextField();
+        jbtConsulta = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jtbDetalle = new javax.swing.JTable();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        total_recaudacion = new javax.swing.JTextField();
+        total_boletos = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         btn_repVenta_salir.setText("Salir");
         btn_repVenta_salir.addActionListener(new java.awt.event.ActionListener() {
@@ -37,23 +66,109 @@ public class GUI_ReporteVentas extends javax.swing.JFrame {
                 btn_repVenta_salirActionPerformed(evt);
             }
         });
+        getContentPane().add(btn_repVenta_salir, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 370, 87, 35));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(281, Short.MAX_VALUE)
-                .addComponent(btn_repVenta_salir, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(32, 32, 32))
+        jPanel1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+
+        jLabel2.setText("Desde:");
+
+        fecha_inicio.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.DateFormatter(new java.text.SimpleDateFormat("yyyy/MM/dd"))));
+
+        jLabel3.setText("Hasta:");
+
+        fecha_final.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.DateFormatter(new java.text.SimpleDateFormat("yyyy/MM/dd"))));
+
+        jbtConsulta.setText("Consultar");
+        jbtConsulta.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbtConsultaActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(fecha_inicio, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(44, 44, 44)
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(fecha_final, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jbtConsulta)
+                .addContainerGap(239, Short.MAX_VALUE))
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(223, Short.MAX_VALUE)
-                .addComponent(btn_repVenta_salir)
-                .addGap(54, 54, 54))
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jLabel2)
+                        .addComponent(fecha_inicio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jLabel3)
+                        .addComponent(fecha_final, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jbtConsulta))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 32, 670, -1));
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        jLabel1.setText("REPORTE DE VENTAS");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 11, -1, -1));
+
+        jtbDetalle.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null}
+            },
+            new String [] {
+                "Fecha", "Cedula", "Cliente", "Ruta", "Pasajes", "Total"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Integer.class, java.lang.Double.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(jtbDetalle);
+
+        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 87, 670, 277));
+
+        jLabel4.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        jLabel4.setText("Total recaudación:");
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 380, -1, 20));
+
+        jLabel5.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        jLabel5.setText("Total boletos vendidos:");
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 380, -1, 20));
+
+        total_recaudacion.setEditable(false);
+        total_recaudacion.setHorizontalAlignment(javax.swing.JTextField.RIGHT);
+        total_recaudacion.setText("0.00");
+        getContentPane().add(total_recaudacion, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 380, 70, -1));
+
+        total_boletos.setEditable(false);
+        total_boletos.setHorizontalAlignment(javax.swing.JTextField.RIGHT);
+        total_boletos.setText("0");
+        getContentPane().add(total_boletos, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 380, 40, 20));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -64,6 +179,83 @@ public class GUI_ReporteVentas extends javax.swing.JFrame {
         b.setVisible(true);
         dispose();
     }//GEN-LAST:event_btn_repVenta_salirActionPerformed
+
+    private void jbtConsultaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtConsultaActionPerformed
+        try {
+            GeneraReporte();
+        } catch (ParseException ex) {
+            Logger.getLogger(GUI_ReporteVentas.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jbtConsultaActionPerformed
+
+    private void GeneraReporte() throws ParseException {
+
+        Vector cabeceras = new Vector();
+
+        cabeceras.addElement("FECHA");
+        cabeceras.addElement("CEDULA");
+        cabeceras.addElement("CLIENTE");
+        cabeceras.addElement("RUTA");
+        cabeceras.addElement("PASAJES");
+        cabeceras.addElement("TOTAL");
+
+        //Crear vector con nombre apellido pasajero cedula edad
+        DefaultTableModel mdlTabla = new DefaultTableModel(cabeceras, 0);
+
+        try {
+            double TotPasajes = 0;
+            double TotBoletos = 0;
+
+            String sFechaInicio = fecha_inicio.getText();
+            String sFechaFinal = fecha_final.getText();
+
+            Date FechaInicio = new SimpleDateFormat("yyyy/MM/dd").parse(sFechaInicio);
+            Date FechaFinal = new SimpleDateFormat("yyyy/MM/dd").parse(sFechaFinal);
+
+            try (FileReader fr = new FileReader(Entorno.RutaBoletos);
+                    BufferedReader br = new BufferedReader(fr)) {
+
+                String d;
+
+                while ((d = br.readLine()) != null) {
+                    StringTokenizer dato = new StringTokenizer(d, "|");
+                    Vector x = new Vector();
+
+                    while (dato.hasMoreTokens()) {
+                        x.addElement(dato.nextToken());
+                    }
+
+                    String a = x.elementAt(2).toString();
+                    Date FechaBoleto = new SimpleDateFormat("yyyy/MM/dd").parse(a);
+
+                    if (FechaBoleto.compareTo(FechaInicio) >= 0 && FechaBoleto.compareTo(FechaFinal) <= 0) {
+
+                        Vector reg = new Vector();
+                        reg.addElement(x.get(2));
+                        reg.addElement(x.get(6));
+                        reg.addElement(x.get(7).toString().concat(" ").concat(x.get(8).toString()));
+                        reg.addElement(x.get(11));
+                        reg.addElement(x.get(1));
+                        reg.addElement(x.get(5));
+
+                        TotBoletos += Double.parseDouble(x.get(1).toString());
+                        TotPasajes += Double.parseDouble(x.get(5).toString());
+
+                        mdlTabla.addRow(reg);
+                    }
+                }
+            }
+
+            jtbDetalle.setModel(mdlTabla);
+
+            total_boletos.setText(String.valueOf(TotBoletos));
+            total_recaudacion.setText(String.valueOf(TotPasajes));
+
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+
+    }
 
     /**
      * @param args the command line arguments
@@ -93,14 +285,25 @@ public class GUI_ReporteVentas extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new GUI_ReporteVentas().setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new GUI_ReporteVentas().setVisible(true);
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btn_repVenta_salir;
+    private javax.swing.JFormattedTextField fecha_final;
+    private javax.swing.JFormattedTextField fecha_inicio;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JButton jbtConsulta;
+    private javax.swing.JTable jtbDetalle;
+    private javax.swing.JTextField total_boletos;
+    private javax.swing.JTextField total_recaudacion;
     // End of variables declaration//GEN-END:variables
 }
